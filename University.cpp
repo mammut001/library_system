15cbeea3616d87da1169f264535b8ccf31f8428c
P A I <dongpei110@gmail.com>
2024-06-22 23:35:54 -0400

    add uni.cc and uni.h

---
+++ University.cpp
@@ -0,0 +1,5 @@
+//
+// Created by doge-to-the-moon on 2024-06-22.
+//
+
+#include "University.h"
---
+++ University.h
@@ -0,0 +1,15 @@
+//
+// Created by doge-to-the-moon on 2024-06-22.
+//
+
+#ifndef LIBRARY_SYSTEM_UNIVERSITY_H
+#define LIBRARY_SYSTEM_UNIVERSITY_H
+
+
+class University {
+
+
+};
+
+
+#endif //LIBRARY_SYSTEM_UNIVERSITY_H
